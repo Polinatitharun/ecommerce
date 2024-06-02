5bf560ef35078e8e72afc99aa30a63eb814cbe97
THARUN POLINATI <tharunpolinati@gmail.com>
2024-06-02 15:09:57 +0530

    Deploying to GitHub Pages"

--- OneDrive/Documents/practice/final/src/App.js
+++ OneDrive/Documents/practice/final/src/App.js
@@ -1,27 +1,20 @@
-import React, { useContext, useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Dashboard from './Dashboard';
-import Cart from './Cart'; // Import Cart component
 import Login from './Login';
 import ProtectedRoute from './ProtectedRoute';
 import { AuthContext } from './AuthContext';
+import Cart from './Cart';
 import './App.css';
 
 const App = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    if (!cart.some(item => item.id === product.id)) {
-      setCart([...cart, product]);
-    }
-  };
 
   return (
-    <div>
+    <Router>
       <div className='rightnav'>
         <nav>
           <ul>
@@ -37,7 +30,7 @@ const App = () => {
                   <Link to="/contact">Contact</Link>
                 </li>
                 <li>
-                  <Link to="/cart">Cart</Link> {/* Correct the link */}
+                  <Link to="/cart">Cart</Link>
                 </li>
                 <li>
                   <button onClick={logout}>Logout</button>
@@ -63,16 +56,16 @@ const App = () => {
         </nav>
 
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<h2>Signup Page</h2>} />
-          <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard addToCart={addToCart} cart={cart} />} />} />
-          <Route path="/cart" element={<ProtectedRoute element={<Cart cart={cart} />} />} /> {/* Add Cart route */}
+          <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+          <Route path="/cart" element={<ProtectedRoute element={<Cart />} />} />
         </Routes>
       </div>
-    </div>
+    </Router>
   );
 };
 
