f06a739071d8f8bc34207f1301da0cd793b53236
THARUN POLINATI <tharunpolinati@gmail.com>
2024-06-02 15:12:23 +0530

    Updates

--- OneDrive/Documents/practice/final/src/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// import {Routes,Route} from "react-router-dom";
-// const Navbar = () => {
-//   return (
-//     <div>Navbar</div>
-//   )
-// }
-
-// export default Navbar
--- OneDrive/Documents/practice/final/src/SIgnup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SIgnup = () => {
-  return (
-    <div>SIgnup</div>
-  )
-}
-
-export default SIgnup
